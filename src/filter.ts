1ab90467760bb4deeadb9a80760d8343569b6311
czl <471060995@qq.com>
2019-12-27 11:31:30 +0800

    add dist files

--- src/filter.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import moment from "moment";
-
-Vue.filter("date", function(value: Date, format: string = "YYYY-MM-DD") {
-  if (!value) {
-    return "";
-  }
-  return moment(value).format(format);
-});
